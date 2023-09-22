e47a9bf49d5c18112fff4d64718eb297f50934c4
Jack Williams <jackmwilliams35@gmail.com>
2023-09-22 15:45:55 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+window.leastFavoriteCustomer = 'initial value';
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(newCustomer) {
+    window.bestCustomer = newCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    throw new Error("Assignment to constant variable.");
+    window.leastFavoriteCustomer = 'new value';
+}
+
